9f06d21a5cbc02656acb1300b697a011640a2962
Andreas Florath <andreas@florath.org>
2010-02-06 20:43:32 +0100

    Added read for Requirement

--- lib/Requirement.py
+++ lib/Requirement.py
@@ -12,7 +12,81 @@ class Requirement:
     def __init__(self, fd, rid):
         self.req = {}
         self.id = rid
+        
+        self.lt_empty = 1
+        self.lt_comment = 2
+        self.lt_continue = 3
+        self.lt_error = 4
+        self.lt_initial = 5
+
         self.read(fd)
 
+    def erase_heading_ws(self, l):
+        while len(l)>0 and l[0]==" ":
+            l = l[1:]
+        return l
+        
+    def parse_line(self, line, lineno):
+        if len(line)>0 and line[-1]=='\n':
+            line = line[:-1]
+        # Empty line
+        if len(line)==0:
+            return self.lt_empty, None, None
+        # Comment line
+        if line[0]=='#':
+            return self.lt_comment, None, None
+        # Continue line
+        if line[0]==' ':
+            return self.lt_continue, None, line
+        # 'Normal' line
+        ls = line.split(":", 1)
+        if len(ls)==0:
+            # No ':' found
+            print("+++ ERROR %s:%d: no ':' in line '%s'" %
+                  (self.id, lineno, line))
+            return self.lt_error, None, None
+        if len(ls[0])==0:
+            # ':' is first char in line
+            print("+++ ERROR %s:%d: no char before ':'" %
+                  (self.id, lineno))
+            return self.lt_error, None, None
+        # Normal 'initial' case
+        return self.lt_initial, ls[0], self.erase_heading_ws(ls[1])
+
     def read(self, fd):
-        pass
+        lineno = 0
+        fine=True
+        last_key = None
+        for line in fd:
+            lineno+=1
+            line_type, key, content = self.parse_line(line, lineno)
+
+            if line_type==self.lt_empty:
+                continue
+            if line_type==self.lt_comment:
+                continue
+            if line_type==self.lt_error:
+                fine=False
+                continue
+            if line_type==self.lt_continue:
+                if last_key==None:
+                    print("+++ ERROR %s:%d: continue line without initial line" %
+                          (self.id, lineno))
+                    fine=False
+                self.req[last_key]+=content
+                continue
+            if line_type==self.lt_initial:
+                if key in self.req:
+                    print("+++ ERROR %s:%d: key '%d' already exists" %
+                          (self.id, lineno, key))
+                    fine=False
+                    continue
+                self.req[key] = content
+                last_key = key
+                continue
+            print("+++ ERROR %s:%d: Invalid line_type '%d'" %
+                  (self.id, lineno, line_type))
+            fine=False
+        return fine
+
+
