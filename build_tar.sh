56e1e1a1efd0bad466dbdff26d6735a467a550d9
Andreas Florath <sf@flonatel.org>
2010-02-11 10:41:22 +0100

    Build the latex and graph file for the release

--- build_tar.sh
+++ build_tar.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-PACKAGE_NAME=rmtoo-0.9
+PACKAGE_NAME=rmtoo-1
+
+./bin/rmtoo -c latex -d doc/requirements -f doc/requirements/Config.py -l doc/latex -m .
+(cd doc/latex && pdflatex requirements.tex ; pdflatex requirements.tex )
+mv doc/latex/requirements.pdf doc
+dot -Tpng -o doc/reqsdepgraph.png doc/latex/reqs/dependsgraph.dot
 
 mkdir -p package/${PACKAGE_NAME}
-for d in bin COPYING doc gpl-3.0.txt rmtoo setenv.sh
+for d in bin COPYING doc gpl-3.0.txt rmtoo setenv.sh Readme.txt
 do
     cp -r $d package/${PACKAGE_NAME}
 done
+
+find package/${PACKAGE_NAME} -name "*~" | xargs rm
 
 (cd package
 tar -cf ${PACKAGE_NAME}.tar ${PACKAGE_NAME}
