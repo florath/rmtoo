139ab3af17a9bfd6b7d2927908c8a11ef2b25376
Andreas Florath <sf@flonatel.org>
2010-02-11 16:14:18 +0100

    Added requirements tag History

---
+++ rmtoo/modules/ReqHistory.py
@@ -0,0 +1,23 @@
+#
+# Requirement Management Toolset
+#
+# (c) 2010 by flonatel
+#
+# For licencing details see COPYING
+#
+
+class ReqHistory:
+    tag = "History"
+
+    def __init__(self, opts, config):
+        self.opts = opts
+        self.config = config
+        
+    def type(self):
+        return "reqtag"
+
+    def rewrite(self, req):
+    	# This is optional
+        if self.tag in req.req:
+            req.t_Hinstory = req.req[self.tag]
+            del req.req[self.tag]
