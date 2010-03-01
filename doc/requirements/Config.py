bb193b3a383367a4e44d19b20b228d5bfde39358
Andreas Florath <sf@flonatel.org>
2010-03-02 00:31:57 +0100

    Added management as stakeholder to rmtoo requirements

--- doc/requirements/Config.py
+++ doc/requirements/Config.py
@@ -6,5 +6,5 @@ class Config:
     # development - team at flonatel
     # users - users from the Internet (sourceforge replies and wishes)
     # customers - people and companies who are flonatel's customers
-    stakeholders = ["development", "users", "customers"]
+    stakeholders = ["development", "management", "users", "customers"]
     inventors = ["flonatel", ]
