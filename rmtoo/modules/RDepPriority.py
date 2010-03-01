fa774f49e03182824932b8ef9b6b12389b5ec2b5
Andreas Florath <sf@flonatel.org>
2010-03-02 00:41:36 +0100

    Implemented new priority computation algorithm

--- rmtoo/modules/RDepPriority.py
+++ rmtoo/modules/RDepPriority.py
@@ -6,8 +6,14 @@
 # For licencing details see COPYING
 #
 
+#
+# This class computes the priority.
+#
+# If one node has priority 0, the whole subtree has also priority 0.
+# A node has always the maximum of possible priorities.
+#
 class RDepPriority:
-    depends_on = ["RDepDependsOn", "RDepMasterNoPrio", "RDepNoDirectedCircles"]
+    depends_on = ["RDepDependsOn", "RDepNoDirectedCircles"]
 
     def __init__(self, opts, config):
         self.opts = opts
@@ -19,3 +25,29 @@ class RDepPriority:
     def set_modules(self, mods):
         self.mods = mods
 
+    # Do a DFS and compute the priority during that way.
+    # If there is a node which was already visited, only recompute the
+    # subtree, if the new priority is higher.
+    def rewrite(self, reqset):
+
+        def prioritize(node, inc_weight):
+            node.tags["Priority"] = node.tags["Factor"] * inc_weight
+
+        # The second argument (the number) is the weight of the
+        # incoming edge.
+        def handle_priorization(node, inc_weight):
+            # This is the weight which is inherited
+            weight = inc_weight * node.tags["Factor"]
+
+            # If there is none, or if the current priority is lower
+            # that the newly computed, recompute this node and
+            # everything beneath.
+            if "Priority" not in node.tags \
+                    or node.tags["Priority"] < weight:
+                node.tags["Priority"] = weight
+                for n in node.incoming:
+                    handle_priorization(n, weight)
+
+        # Start at the root (master) node and evaluate all nodes
+        # there. 
+        handle_priorization(reqset.graph_master_node, 1.0)
