25e505aa9e7dd11cfd8e0093e66d7dc9c0fa9923
Andreas Florath <sf@flonatel.org>
2010-03-06 08:25:45 +0100

    Added new Tag: Effort estimation

---
+++ rmtoo/modules/ReqEffortEst.py
@@ -0,0 +1,39 @@
+#
+# Requirement Management Toolset
+#
+# (c) 2010 by flonatel
+#
+# For licencing details see COPYING
+#
+
+#
+# The 'Effort estimation' must be one of
+#   0, 1, 2, 3, 5, 8, 13
+#
+# (see Roman Pichler 'SCRUM', dpunkt.verlag, 2008)
+
+from rmtoo.lib.ReqTagGeneric import ReqTagGeneric
+
+class ReqEffortEst(ReqTagGeneric):
+    tag = "Effort estimation"
+    valid_values = [0, 1, 2, 3, 5, 8, 13, 21]
+
+    def __init__(self, opts, config):
+        ReqTagGeneric.__init__(self, opts, config)
+
+    def rewrite(self, rid, req):
+    	# This is optional
+        state, tag, value = self.handle_optional_tag(req)
+        if value==None:
+            return state, tag, value
+
+        v = int(value)
+
+        if v not in self.valid_values:
+            print("+++ ERROR %s: effort estimation must be one of %s"
+                  % self.valid_values)
+            return False, None, None
+
+        return True, tag, v
+
+            
