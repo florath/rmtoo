5b04fa1abe1e3ae532c451f930195523be39bb0f
Andreas Florath <sf@flonatel.org>
2010-03-14 21:13:19 +0100

    Removed test temp files

--- tmp/check_continuum.py
+++
@@ -1,7 +0,0 @@
-
-from rmtoo.lib.ReqsContinuum import ReqsContinuum
-
-rc = ReqsContinuum("/home/florath/devel/rmtoo/doc/requirements",
-                   "be2cf1bd0bf354dc70ade2ecb82258d0d8b63126")
-
-rc = ReqsContinuum("/home/florath/devel/rmtoo/doc/requirements")
