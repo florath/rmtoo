b4efcbc9ceeddc9c11d54942f99a417a5ac39f39
Andreas Florath <sf@flonatel.org>
2010-07-31 15:12:59 +0200

    Add infrastructure for analytics

---
+++ rmtoo/lib/Analytics.py
@@ -0,0 +1,19 @@
+#
+# Requirement Management Toolset: Analytics
+#
+#  Some parts of the requirements management can be automatically
+#  checked.  This class handles the analytics to the different
+#  analytics modules.
+#
+# (c) 2010 by flonatel
+#
+# For licencing details see COPYING
+#
+
+class Analytics:
+
+    # The argument to the analytics modules is the (latest) set of
+    # requirments.  (It makes sense only to check them.)
+    @staticmethod
+    def run(reqs):
+        pass
