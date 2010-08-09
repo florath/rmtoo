1b8bd0c11114640fac62984e42b1a86b4e164a07
Andreas Florath <sf@flonatel.org>
2010-08-09 21:27:42 +0200

    Bumped up version number to 11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 import os, sys
 
 package = 'rmtoo'
-version = '10'
+version = '11'
 
 def adjust(input, output):
     if os.path.exists(output):
@@ -27,6 +27,7 @@ setup(name=package, version=version,
       url='http://rmtoo.gnu4u.org',
       packages=['rmtoo', 'rmtoo/lib', 'rmtoo/lib/digraph',
                 'rmtoo/modules', 'rmtoo/tests',
+                'rmtoo/lib/analytics',
                 'rmtoo/tests/lib', 'rmtoo/tests/syntax-test',
                 'rmtoo/tests/unit-test', 'rmtoo/tests/unit-test/tag-tests',
                 'rmtoo/tests/unit-test/digraph-test',
@@ -40,6 +41,54 @@ setup(name=package, version=version,
                 'rmtoo/tests/unit-test/topic-tests/testdata',
                 'rmtoo/tests/unit-test/topic-tests/testdata/topicset01',
                 'rmtoo/tests/output-test',
+
+                # Blackbox Tests
+                'rmtoo/tests/blackbox-test',
+                'rmtoo/tests/blackbox-test/bb001-test',
+                'rmtoo/tests/blackbox-test/bb001-test/input',
+                'rmtoo/tests/blackbox-test/bb001-test/input/reqs',
+                'rmtoo/tests/blackbox-test/bb001-test/input/topics',
+                'rmtoo/tests/blackbox-test/bb001-test/result_is',
+                'rmtoo/tests/blackbox-test/bb001-test/result_should',
+                'rmtoo/tests/blackbox-test/bb001-test/result_should/html',
+                'rmtoo/tests/blackbox-test/bb002-test',
+                'rmtoo/tests/blackbox-test/bb002-test/input',
+                'rmtoo/tests/blackbox-test/bb002-test/input/reqs',
+                'rmtoo/tests/blackbox-test/bb002-test/input/topics',
+                'rmtoo/tests/blackbox-test/bb002-test/result_is',
+                'rmtoo/tests/blackbox-test/bb002-test/result_should',
+                'rmtoo/tests/blackbox-test/bb002-test/result_should/html',
+                'rmtoo/tests/blackbox-test/bb003-test',
+                'rmtoo/tests/blackbox-test/bb003-test/input',
+                'rmtoo/tests/blackbox-test/bb003-test/input/reqs',
+                'rmtoo/tests/blackbox-test/bb003-test/input/topics',
+                'rmtoo/tests/blackbox-test/bb003-test/result_is',
+                'rmtoo/tests/blackbox-test/bb003-test/result_should',
+                'rmtoo/tests/blackbox-test/bb003-test/result_should/html',
+                'rmtoo/tests/blackbox-test/bb004-test',
+                'rmtoo/tests/blackbox-test/bb004-test/input',
+                'rmtoo/tests/blackbox-test/bb004-test/input/reqs',
+                'rmtoo/tests/blackbox-test/bb004-test/input/topics',
+                'rmtoo/tests/blackbox-test/bb004-test/result_is',
+                'rmtoo/tests/blackbox-test/bb004-test/result_should',
+                'rmtoo/tests/blackbox-test/bb004-test/result_should/html',
+
+                ## contrib
+                'rmtoo/contrib'
+                # async
+                'rmtoo/contrib/async-0.6.1',
+                'rmtoo/contrib/async-0.6.1/mod',
+                # gitdb
+                'rmtoo/contrib/gitdb-0.5.1',
+                'rmtoo/contrib/gitdb-0.5.1/db',
+                # git-python
+                'rmtoo/contrib/GitPython-0.3.0-beta2',
+                'rmtoo/contrib/GitPython-0.3.0-beta2/lib',
+                'rmtoo/contrib/GitPython-0.3.0-beta2/lib/GitPython.egg-info',
+                'rmtoo/contrib/GitPython-0.3.0-beta2/lib/git',
+                'rmtoo/contrib/GitPython-0.3.0-beta2/lib/git/repo',
+                'rmtoo/contrib/GitPython-0.3.0-beta2/lib/git/index',
+                'rmtoo/contrib/GitPython-0.3.0-beta2/lib/git/objects',
                 ],
       license="GPL V3",
       platforms="all",
