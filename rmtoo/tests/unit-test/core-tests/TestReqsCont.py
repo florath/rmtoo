1551a5a2ae62d26ed688544f3a3ddbcaac35b2b2
Andreas Florath <sf@flonatel.org>
2010-08-19 18:15:16 +0200

    Add test case for ReqsContinuum

--- rmtoo/tests/unit-test/core-tests/TestReqsCont.py
+++ rmtoo/tests/unit-test/core-tests/TestReqsCont.py
@@ -8,9 +8,24 @@
 # For licencing details see COPYING
 #
 
+import tempfile
+import shutil
+
 from rmtoo.lib.ReqsContinuum import ReqsContinuum
+from rmtoo.lib.RMTException import RMTException
 
 class TestOutputHandler:
 
     def test_positive_01(self):
-        pass
+        try:
+            tdir = tempfile.mkdtemp(prefix="rmtoo-tst-ctnt-")
+
+            class Config:
+                reqs_spec = {"commit_interval": ["v1", "v7"],
+                             "directory": tdir }
+                
+            ReqsContinuum(None, None, Config)
+            assert(False)
+        except RMTException, rmte:
+            shutil.rmtree(tdir)
+            assert(rmte.id()==40)
