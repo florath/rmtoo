b9b8bd8a1bad7aef1975e165ea1d73186fd2a6c1
Andreas Florath <sf@flonatel.org>
2010-09-08 19:09:27 +0200

    Fixed 3052358: specific error is now printed in case of unexistant referenced topic

---
+++ rmtoo/tests/lib/ModuleHelper.py
@@ -0,0 +1,14 @@
+#
+# Helper for testing modules and related classes
+#
+# (c) 2010 by flonatel
+#
+# For licencing details see COPYING
+#
+
+def mods_list(lm, mod_base_dir):
+    mods_name = mod_base_dir.split("/")
+    ml=["rmtoo"]
+    ml.extend(mods_name)
+    ml.append(lm)
+    return ml
---
+++ rmtoo/tests/unit-test/core-tests/testdata/modules08/Module01.py
@@ -0,0 +1,25 @@
+#
+# Requirement Management Toolset
+#  Test Module
+#
+# (c) 2010 by flonatel
+#
+# For licencing details see COPYING
+#
+
+from rmtoo.lib.digraph.Digraph import Digraph
+
+class Module01(Digraph.Node):
+    depends_on = []
+
+    def __init__(self, opts, config):
+        Digraph.Node.__init__(self, "Module01")
+
+    def type(self):
+        return "reqdeps"
+
+    def set_modules(self, mods):
+        pass
+
+    def rewrite(self,reqset):
+        return True
