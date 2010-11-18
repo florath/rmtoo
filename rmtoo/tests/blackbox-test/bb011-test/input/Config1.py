3350842b7c1210d697eb4981ee8a519d0abad963
Andreas Florath <sf@flonatel.org>
2010-11-18 08:49:34 +0100

    Added test case when ignoring requireemnts because the topic is not used

---
+++ rmtoo/tests/blackbox-test/bb011-test/input/Config1.py
@@ -0,0 +1,38 @@
+import os
+
+class Config:
+
+    basedir = "tests/blackbox-test/bb009-test/"
+    result_is = os.environ["rmtoo_test_dir"]
+
+    stakeholders = ["development", "management", "users", "customers"]
+
+    inventors = ["flonatel", ]
+
+    reqs_spec = \
+        {
+           "directory": basedir + "input/reqs",
+           "commit_interval": ["FILES", 
+                               "FILES"],
+           #["138be32f1985aec694934263f02e47292deaac91", "FILES"]
+           #["v8", "FILES"]
+           # ["FILES", "FILES"]
+           "default_language": "en_GB",
+        }
+
+    topic_specs = \
+        {
+          "ts_common": [basedir + "input/topics", "B1"],
+        }
+
+    analytics_specs = \
+        { 
+           "stop_on_errors": True,
+           "topics": "ts_common",
+        }
+    
+    output_specs = \
+        [ 
+          ["graph2",
+           ["ts_common", result_is + "/req-graph2.dot"]],
+        ]
