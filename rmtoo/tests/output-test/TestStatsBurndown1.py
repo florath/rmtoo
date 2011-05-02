c585c059a58198f9348d524de571c684c74de31a
Andreas Florath <sf@flonatel.org>
2011-05-02 09:01:35 +0200

    Added empty tests

---
+++ rmtoo/tests/output-test/TestStatsBurndown1.py
@@ -0,0 +1,16 @@
+#
+# rmtoo 
+#   Free and Open Source Requirements Management Tool
+#
+# Unit test for RequirementSet
+#
+# (c) 2010-2011 on flonatel
+#
+# For licencing details see COPYING
+#
+
+from rmtoo.outputs.stats_burndown1 import stats_burndown1
+
+class TestStatsBurndown1:
+    pass
+
---
+++ rmtoo/tests/output-test/TestStatsSprintBurndown1.py
@@ -0,0 +1,16 @@
+#
+# rmtoo 
+#   Free and Open Source Requirements Management Tool
+#
+# Unit test for RequirementSet
+#
+# (c) 2010-2011 on flonatel
+#
+# For licencing details see COPYING
+#
+
+from rmtoo.outputs.stats_sprint_burndown1 import stats_sprint_burndown1
+
+class TestStatsSprintBurndown1:
+    pass
+
