05a1f4f14346fc2871cb25150e9ddc0b0f2ea39b
Andreas Florath <sf@flonatel.org>
2012-02-10 18:17:55 +0100

    First try to get config tmp directory handling sorted

--- rmtoo/tests/blackbox-test/bb001-test/input/Config1.py
+++ rmtoo/tests/blackbox-test/bb001-test/input/Config1.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
 
-    basedir = "tests/blackbox-test/bb001-test/"
-    result_is = os.environ["rmtoo_test_dir"]
+    #basedir = "tests/blackbox-test/bb001-test/"
+    #result_is = os.environ["rmtoo_test_dir"]
 
     stakeholders = ["development", "management", "users", "customers"]
 
@@ -11,7 +11,7 @@ class Config:
 
     reqs_spec = \
         {
-           "directory": basedir + "input/reqs",
+           "directory": "${GET:basedir}/input/reqs",
            "commit_interval": ["v10",
                                "c35f8e918ab1d05ab3e2ba0be52a3f4092035663"],
            #["138be32f1985aec694934263f02e47292deaac91", "FILES"]
@@ -22,7 +22,7 @@ class Config:
 
     topic_specs = \
         {
-          "ts_common": [basedir + "input/topics", "ReqsDocument"],
+          "ts_common": ["${GET:basedir}/input/topics", "ReqsDocument"],
         }
 
     analytics_specs = \
@@ -34,29 +34,29 @@ class Config:
     output_specs = \
         [
           ["prios",
-           ["ts_common", result_is + "/reqsprios.tex",
+           ["ts_common", "${ENV:rmtoo_test_dir}/reqsprios.tex",
             {"start_date": "2011-04-26"} ]],
 
           ["graph",
-           ["ts_common", result_is + "/req-graph1.dot"]],
+           ["ts_common", "${ENV:rmtoo_test_dir}/req-graph1.dot"]],
 
           ["graph2",
-           ["ts_common", result_is + "/req-graph2.dot"]],
+           ["ts_common", "${ENV:rmtoo_test_dir}/req-graph2.dot"]],
 
           ["stats_reqs_cnt",
-           ["ts_common", result_is + "/stats_reqs_cnt.csv"]],
+           ["ts_common", "${ENV:rmtoo_test_dir}/stats_reqs_cnt.csv"]],
 
           ["latex2",
-           ["ts_common", result_is + "/reqtopics.tex"]],
+           ["ts_common", "${ENV:rmtoo_test_dir}/reqtopics.tex"]],
 
           ["html",
            ["ts_common",
-            result_is + "/html", basedir + "input/header.html",
-            basedir + "input/footer.html"]],
+            "${ENV:rmtoo_test_dir}/html", "${GET:basedir}/input/header.html",
+            "${GET:basedir}/input/footer.html"]],
 
           ["xml_ganttproject_2",
-           ["ts_common", result_is + "/gantt2.xml", 1]],
+           ["ts_common", "${ENV:rmtoo_test_dir}/gantt2.xml", 1]],
 
           ["oopricing1",
-           ["ts_common", result_is + "/reqspricing"]],
+           ["ts_common", "${ENV:rmtoo_test_dir}/reqspricing"]],
         ]
