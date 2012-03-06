37de13b05ece7b6cbdfe77bcd2a1868f57c4cf36
Andreas Florath <sf@flonatel.org>
2012-03-06 17:22:51 +0100

    Code cleanup - removed unused code

--- rmtoo/lib/StringHelper.py
+++ rmtoo/lib/StringHelper.py
@@ -1,21 +1,22 @@
-#
-# rmtoo
-#   Free and Open Source Requirements Management Tool
-#
-# StringHelper
-#   Small helper and utils for the string class
-#
-# (c) 2011 by flonatel
-#
-# For licencing details see COPYING
-#
+'''
+ rmtoo
+   Free and Open Source Requirements Management Tool
+   
+  StringHelper
+   Small helper and utils for the string class
+   
+ (c) 2011-2012 by flonatel GmbH & Co. KG
 
+ For licensing details see COPYING
+'''
+
+# pylint: disable=W0232
 class StringHelper:
 
-    # This is mostly the same as the string 'join()' method, but
-    # The delimiter is also added to the end of the list.
     @staticmethod
     def join_ate(delim, jlist):
+        '''This is mostly the same as the string 'join()' method, but
+           The delimiter is also added to the end of the list.'''
         r = ""
         for j in jlist:
             r += j + delim
--- rmtoo/lib/Topic.py
+++ rmtoo/lib/Topic.py
@@ -10,12 +10,9 @@
  For licensing details see COPYING
 '''
 
-import os
-
 from rmtoo.lib.storagebackend.txtfile.TxtRecord import TxtRecord
 from rmtoo.lib.digraph.Digraph import Digraph
 from rmtoo.lib.RMTException import RMTException
-from rmtoo.lib.storagebackend.txtfile.TxtIOConfig import TxtIOConfig
 from rmtoo.lib.logging.EventLogging import tracer
 from rmtoo.lib.FuncCall import FuncCall
 
@@ -68,6 +65,7 @@ class Topic(Digraph.Node):
         Digraph.Node.__init__(self, tname)
         # This is the name of the topic (short description)
         self.__topic_name = None
+        self.__tags = None
         self._config = config
         tracer.debug("Called: name [%s]." % tname)
         self.__digraph = digraph
@@ -135,80 +133,3 @@ class Topic(Digraph.Node):
         '''Returns the topic id.
            Note: This is NOT the short description - a la 'Name'.'''
         return self.name
-
-    def UNUSED__init__(self, tdir, tname, dg, txtioconfig, cfg, tlevel=0,
-                 tsuper=None):
-        tracer.info("called: directory [%s] name [%s]" % (tdir, tname))
-        Digraph.Node.__init__(self, tname)
-        self.dir = tdir
-        # Master map is needed for deciping requirements into the
-        # appropriate topic.
-        self.digraph = dg
-        self.txtioconfig = txtioconfig
-        self.cfg = cfg
-        # Idendation level of this topic
-        self.level = tlevel
-        self.super = tsuper
-        # This is a list of requirements which contain to this topic.
-        self.reqs = []
-        # The name of the Topic is mandatory (and also position
-        # independent)
-        # Note: there is also a .name field inherited from the
-        # Digraph.Node (which hold in this case the topic's id).
-        self.topic_name = None
-
-        # This must only be done if there is a directory given
-        if self.dir != None:
-            self.read()
-            self.extract_name()
-        else:
-            # In this case the tag list is (initally) empty
-            self.t = []
-
-    def UNUSED___str__(self):
-        return "name [" + self.name + "]"
-
-    # Extract the name from the list (it's mandatory!)
-    def UNUSED_extract_name(self):
-        for nt in self.t:
-            if nt.get_tag() == "Name":
-                self.topic_name = nt.get_content()
-                del(nt)
-                return
-        raise RMTException(62, "Mandatory tag 'Name' not given in topic",
-                           self.name)
-
-    # Create Makefile Dependencies
-    def UNUSED_cmad(self, reqscont, ofile, tname):
-        reqs_dir = reqscont.config.get_value('requirements.input.directory')
-        for req in self.reqs:
-            # Add all the included requirements
-            ofile.write(" %s.req" %
-                         os.path.join(reqs_dir, req.name))
-        # Add all the subtopics
-        for n in self.outgoing:
-            ofile.write(" ${TOPIC_%s_%s_DEPS}" % (tname, n.name))
-
-    # Read in a specific topic
-    def UNUSED_read(self):
-        self.digraph.add_node(self)
-        fd = file(os.path.join(self.dir, self.name + ".tic"))
-        self.t = TxtRecord.from_fd(fd, self.name, self.txtioconfig)
-        for tag in self.t:
-            # If the topic has subtopics, read them also in.
-            if tag.get_tag() == "SubTopic":
-                ntopic = Topic(self.dir, tag.get_content(), self.digraph,
-                               self.txtioconfig, self.cfg, self.level + 1,
-                               self)
-                #self.outgoing.append(ntopic)
-                Digraph.create_edge(self, ntopic)
-                #self.outgoing.append(ntopic)
-        fd.close()
-
-    def UNUSED_add_req(self, req):
-        self.reqs.append(req)
-
-    # Returns the name of the game
-    def UNUSED_get_name(self):
-        return self.topic_name
-
