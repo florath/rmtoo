d802905c388ffd423b0b4bf6ecbe80a1327d2ce0
Kristoffer Nordström <kristoffer.nordstroem@gmail.com>
2017-06-03 00:45:35 +0200

    Preliminary working with old values.

--- rmtoo/outputs/LatexJinja2.py
+++ rmtoo/outputs/LatexJinja2.py
@@ -13,6 +13,7 @@ from __future__ import unicode_literals
 import io
 import jinja2
 from six import iteritems
+from contextlib import suppress
 
 from rmtoo.lib.Constraints import Constraints
 from rmtoo.lib.TestCases import collect
@@ -46,7 +47,7 @@ class LatexJinja2(StdOutputParams, ExecutorTopicContinuum, CreateMakeDependencie
         self.__constraints_reqs_ref = {}
 
         # Jinja2 initialisation
-        templateLoader = jinja2.FileSystemLoader(searchpath="./latex/LatexJinja2/")
+        templateLoader = jinja2.FileSystemLoader(searchpath=oconfig['template_path'])
         template_env_unmodded = jinja2.Environment(loader=templateLoader)
         self._template_env = template_env_unmodded.overlay(
             block_start_string='((*',
@@ -213,42 +214,53 @@ class LatexJinja2(StdOutputParams, ExecutorTopicContinuum, CreateMakeDependencie
                          'description':  req.get_value("Description").get_content(),
                          'req_status': req.get_value("Status").get_output_string()}
 
+        if req.is_val_av_and_not_null("Rationale"):
+            template_vars['rationale'] = req.get_value("Rationale").get_content()
+        if req.is_val_av_and_not_null("Note"):
+            template_vars['note'] = req.get_value("Note").get_content()
+
+        if len(req.outgoing) > 0:
+            # Create links to the corresponding dependency nodes.
+            inc = [d.get_id() for d in sorted(req.outgoing, key=lambda r: r.get_id())]
+            template_vars['solvedby'] = inc
+
+        if len(req.incoming) > 0:
+            # Only output the depends on when there are fields for output.
+            inc = [d.get_id() for d in sorted(req.incoming, key=lambda r: r.get_id())]
+            template_vars['dependson'] = inc
+
+        try:
+            template_vars['status'] = req.get_value("Status").get_output_string()
+        except KeyError:
+            pass
+        try:
+            template_vars['clstr'] = req.get_value("Class").get_output_string()
+        except KeyError: 
+            pass
+        try:
+            template_vars['rtype'] = req.get_value("Type").as_string()
+        except KeyError:
+            pass
+        try:
+            template_vars['prio'] = req.get_value("Priority") * 10
+        except KeyError:
+            pass
+        try:
+            template_vars['owner'] = req.get_value("Owner")
+        except KeyError:
+            pass
+        try:
+            template_vars['inventedon'] = req.get_value("Invented on").strftime("%Y-%m-%d")
+        except KeyError:
+            pass
+        try:
+            template_vars['inventedby'] = req.get_value("Invented by")
+        except KeyError: pass
+
         self.__fd.write(req_template.render(template_vars))
 
 
     def requirement_old_unused(self, req):
-        if req.is_val_av_and_not_null("Rationale"):
-            self.__fd.write(u"\n\\textbf{Rationale:} %s\n"
-                            % req.get_value("Rationale").get_content())
-
-        if req.is_val_av_and_not_null("Note"):
-            self.__fd.write(u"\n\\textbf{Note:} %s\n"
-                            % req.get_value("Note").get_content())
-
-        # Only output the depends on when there are fields for output.
-        if len(req.incoming) > 0:
-            # Create links to the corresponding labels.
-            self.__fd.write(u"\n\\textbf{Depends on:} ")
-            self.__fd.write(u", ".join(
-                ["\\ref{%s} \\nameref{%s}" %
-                 (LatexJinja2.__strescape(d.get_id()),
-                  LatexJinja2.__strescape(d.get_id()))
-                 for d in sorted(req.incoming,
-                                 key=lambda r: r.get_id())]))
-            self.__fd.write(u"\n")
-
-        if len(req.outgoing) > 0:
-            # Create links to the corresponding dependency nodes.
-            self.__fd.write(u"\n\\textbf{Solved by:} ")
-            # No comma at the end.
-            self.__fd.write(u", ".join(
-                ["\\ref{%s} \\nameref{%s}" %
-                 (LatexJinja2.__strescape(d.get_id()),
-                  LatexJinja2.__strescape(d.get_id()))
-                 for d in sorted(req.outgoing,
-                                 key=lambda r: r.get_id())]))
-            self.__fd.write(u"\n")
-
         if self.__ce3set is not None:
             cnstrt = self.__ce3set.get(req.get_id())
             if cnstrt is not None and len(cnstrt) > 0:
@@ -282,52 +294,6 @@ class LatexJinja2(StdOutputParams, ExecutorTopicContinuum, CreateMakeDependencie
             self.__fd.write(u", ".join(tcout))
             self.__fd.write(u"\n")
 
-        status = req.get_value("Status").get_output_string()
-        clstr = req.get_value("Class").get_output_string()
-        rtype = req.get_value("Type").as_string()
-
-        self.__fd.write(u"\n\\par\n{\small \\begin{center}"
-                        "\\begin{tabular}{rlrlrl}\n")
-
-        # Put mostly three things in a line.
-        i = 0
-        for rattr in self._config.get_value("req_attributes"):
-            if rattr == "Id":
-                self.__fd.write(u"\\textbf{Id:} & %s " % req.get_id())
-            elif rattr == "Priority":
-                self.__fd.write(u"\\textbf{Priority:} & %4.2f "
-                                % (req.get_value("Priority") * 10))
-            elif rattr == "Owner":
-                self.__fd.write(u"\\textbf{Owner:} & %s" %
-                                req.get_value("Owner"))
-            elif rattr == "Invented on":
-                self.__fd.write(u"\\textbf{Invented on:} & %s "
-                                % req.get_value("Invented on")
-                                .strftime("%Y-%m-%d"))
-            elif rattr == "Invented by":
-                self.__fd.write(u"\\textbf{Invented by:} & %s "
-                                % req.get_value("Invented by"))
-            elif rattr == "Status":
-                self.__fd.write(u"\\textbf{Status:} & %s " % status)
-            elif rattr == "Class":
-                self.__fd.write(u"\\textbf{Class:} & %s " % clstr)
-            elif rattr == "Type":
-                self.__fd.write(u"\\textbf{Type:} & %s " % rtype)
-            else:
-                # This only happens when a wrong configuration is supllied.
-                raise RMTException(85, "Wrong LatexJinja2 output configuration "
-                                   "supplied: unknown tag [%s]" % rattr)
-            i += 1
-            if i == 3:
-                i = 0
-                self.__fd.write(u"\\\ \n")
-            else:
-                self.__fd.write(u" & ")
-        while i < 2:
-            self.__fd.write(u"& & ")
-            i += 1
-
-        self.__fd.write(u"\end{tabular}\end{center} }\n\n")
 
     def cmad_topic_continuum_pre(self, _):
         '''Write out the one and only dependency to all the requirements.'''
